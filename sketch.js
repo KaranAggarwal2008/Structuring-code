796c92ba59e6ff7ffeac80b2cab54c31744db5ae
Karan Aggarwal <Karanaggarwal2008@gmail.com>
2020-11-24 19:04:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas,backGroundIMG;
+var gameState = 0;
+var playerCount;
+var database;
+var form,game, player;
+
+function setup(){
+  canvas = createCanvas(400,400)
+  database=firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+function draw(){
+
+}
